a5381c036bfd5a37783f18a705b54f123277c754
Finn <derfinn@gmail.com>
2014-08-14 23:00:33 +0200

    optional postgres store

--- Readme.md
+++ Readme.md
@@ -3,4 +3,17 @@
 Small fun module for exploring the `fs-blob-store` API that allows uploading
 gifs and share them with your friends.
 
+## Get it to work
+
+```
+git clone https://github.com/finnp/gif-sharer
+cd gif-sharer
+npm install
+npm start
+```
+If you have a `DATABASE_URL` containing a postgres url in the env, it will
+try to use this for storage, otherwise it will use the filesystem. You can also
+specify the port with `PORT`.
+
+
 ![Cat gif](cat.gif)
--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-var blobs = require('fs-blob-store')
 var Busboy = require('busboy')
 var http = require('http')
 var fs = require('fs')
 
-var store = blobs({path: './data'})
+var store
+if(process.env.DATABASE_URL) {
+  store = require('postgres-blob-store')({url: process.env.DATABASE_URL})
+} else {
+  store = require('fs-blob-store')({path: './data'})
+}
 
 var server = http.createServer(function (req, res) {
   if(req.method === 'POST') {
     // upload picture
     res.setHeader('Content-Type', 'text/html')
     var form = new Busboy({headers: req.headers})
-    var save = store.createWriteStream()
-    save.on('finish', function () {
-      res.end('Right click and copy this image url to share it <br> <img src="/' + save.hash +'" alt="gif"/>')
+    var save = store.createWriteStream(function (err, data) {
+      res.end('Right click and copy this image url to share it <br> <img src="/' + data.hash +'" alt="gif"/>')
     })
     
     form.on('file', function (fieldname, file, filename, encoding, mimetype) {
       if(mimetype === 'image/gif') {
+        console.log('Incoming file ' + filename)
         file.pipe(save)
       } else {
         res.writeHead(406)
